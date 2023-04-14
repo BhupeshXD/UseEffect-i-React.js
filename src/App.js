65e72a2883fedae87512b0d0132ffd4af9eb1ed3
BhupeshXD <bhupeshdhapola7@gmail.com>
2023-04-14 18:36:19 +0530

    UseEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
+import {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const[text, setText] = useState('');        
+  const[name, setName] = useState('love');   // dependency is in bracket which is love
+
+
+  // variation 1 => On every render or changes useEffect hook will bw used
+  // useEffect(()=>{
+  //   console.log("Ui Rendering done");
+  // });
+
+  //varaition -2 ->  first render
+  useEffect(()=>{                      //callback function
+    console.log("Ui Rendering done");
+  },[]);
+  
+
+  //variation 3 => first render + whenever dependeny changes
+  useEffect(()=>{
+    console.log("Change Observed");
+  },[name]);
+
+
+
+
+  const changeHandler =(event)=>{
+    console.log(text);
+    setText(event.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type = "text" onChange={changeHandler}></input>
     </div>
   );
 }
